17f0c186e2199a0156fe243e5127af02c632a968
Gabe <gabeorlanski@gmail.com>
2022-01-28 11:45:48 -0500

    New Jobs

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Queue Slurm Jobs
 
-sbatch --job-name=codeparrot train.sbatch CodeParrot1500M mbpp lvwerra/codeparrot lm
-sbatch --job-name=gptneo train.sbatch GPTNeo1300M mbpp EleutherAI/gpt-neo-1.3B lm
-sbatch --job-name=gpt2 train.sbatch GPTNeo mbpp EleutherAI/gpt-neo-1.3B lm
+sbatch --job-name=codeparrot train.sbatch CodeParrot1500M mbpp \
+  lvwerra/codeparrot lm \
+  training.gradient_accumulation_steps=4
+sbatch --job-name=gpt2 train.sbatch GPT2_1588M mbpp gpt2-xl lm \
+  training.gradient_accumulation_steps=4
