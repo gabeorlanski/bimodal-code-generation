8b3772713cfd453cc9d2fe98b004d8aa4af0c18d
Gabe <gabeorlanski@gmail.com>
2022-01-30 10:30:35 -0500

    I did not in fact, fix the issues

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Queue Slurm Jobs
 
-bash scripts/single_job.sh codeparrot MBPP CodeParrot mbpp lvwerra/codeparrot lm
-bash scripts/single_job.sh gptneo MBPP GPTNeo mbpp EleutherAI/gpt-neo-1.3B lm
-bash scripts/single_job.sh gpt2 MBPP GPT2 mbpp gpt2-xl lm
-bash scripts/single_job.sh codeparrot_small MBPP CodeParrotSmall mbpp \
+bash scripts/train_job.sh codeparrot MBPP CodeParrot mbpp lvwerra/codeparrot lm
+bash scripts/train_job.sh gptneo MBPP GPTNeo mbpp EleutherAI/gpt-neo-1.3B lm
+bash scripts/train_job.sh gpt2 MBPP GPT2 mbpp gpt2-xl lm
+bash scripts/train_job.sh codeparrot_small MBPP CodeParrotSmall mbpp \
   lvwerra/codeparrot-small lm training.batch_size=4
