136cb4e2c0fa96206ff754dc1f974bf64b83e9f3
Gabe <gabeorlanski@gmail.com>
2022-01-30 10:35:09 -0500

    idk

--- scripts/train_job.sh
+++ scripts/train_job.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Queue Single Slurm
 
-jid1=$(sbatch --job-name=$1 train.sbatch $3 $4 $5 $6 ${@:7})
-echo $jid1
-sbatch --job-name=$1 --dependency=afterok:$jid1 eval.sbatch best_models/$2.$3/ validation,test 25
+train_jid=$(sbatch --parsable --job-name=$1 train.sbatch $3 $4 $5 $6 ${@:7})
+echo "Submitted Train $train_jid"
+eval_jid=$(sbatch --parsable --job-name=$1 --dependency=afterok:$train_jid eval.sbatch best_models/$2.$3/ validation,test 25)
+echo "Submitted Eval $eval_jid to run after $train_jid"
