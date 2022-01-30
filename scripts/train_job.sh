b08e305c3a6d2214ad3d269030d59046e37725f4
Gabe <gabeorlanski@gmail.com>
2022-01-30 10:32:53 -0500

    test

--- scripts/train_job.sh
+++ scripts/train_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Queue Single Slurm
 
-jid = $(sbatch --job-name=$1 train.sbatch $3 $4 $5 $6 ${@:7})
-echo $jid
-sbatch --job-name=$1 --dependency=afterok:$jid eval.sbatch best_models/$2.$3/ validation,test 25
+jid1=$(sbatch --job-name=$1 train.sbatch $3 $4 $5 $6 ${@:7})
+echo $jid1
+sbatch --job-name=$1 --dependency=afterok:$jid1 eval.sbatch best_models/$2.$3/ validation,test 25
