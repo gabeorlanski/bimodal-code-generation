2b5fa3c942d95f17aa1a79d4cc7c55352a4b8e63
Gabe <gabeorlanski@gmail.com>
2022-02-01 19:41:21 -0500

    Typo

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -2,13 +2,13 @@
 # Queue Slurm Jobs
 
 parse_jid=$(sbatch --parsable parse_so.sbatch)
-$pretrain_jid=$(sbatch --parsable --dependency=afterok:$parse_jid --job-name=negcodeparrot \
+$pre_jid=$(sbatch --parsable --dependency=afterok:$parse_jid --job-name=negcodeparrot \
   train.sbatch NegativeSOCodeParrot so lvwerra/codeparrot lm pretrain_config \
   training.max_steps=10000 training.log_steps=50 training.save_steps=250 training.eval_steps=250)
-echo "Submitted PreTrain $1(id=$pretrain_jid)"
-train_jid=$(sbatch --parsable --dependency=afterok:$pretrain_jid --job-name=negcodeparrot \
+echo "Submitted PreTrain (id=$pretrain_jid)"
+train_jid=$(sbatch --parsable --dependency=afterok:$pre_jid --job-name=negcodeparrot1 \
   train.sbatch NegativeSOCodeParrot mbpp lvwerra/codeparrot lm \
   is_checkpoint=True +model_path=best_models/SO.NegativeSOCodeParrot)
-echo "Submitted Train $1(id=$train_jid)"
-eval_jid=$(sbatch --parsable --job-name=negcodeparrot --dependency=afterok:$train_jid eval.sbatch best_models/MBPP.NegativeSOCodeParrot/ validation,test 25)
+echo "Submitted Train (id=$train_jid)"
+eval_jid=$(sbatch --parsable --job-name=negcodeparrot2 --dependency=afterok:$train_jid eval.sbatch best_models/MBPP.NegativeSOCodeParrot/ validation,test 25)
 echo "Submitted Eval $eval_jid to run after $1(id=$train_jid)"
