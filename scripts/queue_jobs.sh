bba1e4bd64d050257c4eb6ec477647299c9e3df4
Gabe <gabeorlanski@gmail.com>
2022-02-01 19:17:03 -0500

    Typo

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -2,6 +2,6 @@
 # Queue Slurm Jobs
 
 parse_jid=$(sbatch parse_so.sbatch)
-sbatch --parsable --dependency=afterok:parse_jid --job-name=negcodeparrot \
+sbatch --parsable --dependency=afterok:$parse_jid --job-name=negcodeparrot \
   train.sbatch NegativeSOCodeParrot so lvwerra/codeparrot lm pretrain_config \
   training.max_steps=10000 training.log_steps=50 training.save_steps=250 training.eval_steps=250
