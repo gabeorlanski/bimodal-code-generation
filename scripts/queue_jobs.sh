c1b0eac5d35d47794d3738ebdaedf078cd2d0486
Gabe <gabeorlanski@gmail.com>
2022-02-01 19:42:01 -0500

    ty[p

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -2,7 +2,7 @@
 # Queue Slurm Jobs
 
 parse_jid=$(sbatch --parsable parse_so.sbatch)
-$pre_jid=$(sbatch --parsable --dependency=afterok:$parse_jid --job-name=negcodeparrot \
+pre_jid=$(sbatch --parsable --dependency=afterok:$parse_jid --job-name=negcodeparrot \
   train.sbatch NegativeSOCodeParrot so lvwerra/codeparrot lm pretrain_config \
   training.max_steps=10000 training.log_steps=50 training.save_steps=250 training.eval_steps=250)
 echo "Submitted PreTrain (id=$pretrain_jid)"
