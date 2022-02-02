117fc105df8dd8572bf2f40fee021468c0648d6b
Gabe <gabeorlanski@gmail.com>
2022-02-02 06:47:06 -0500

    minor change

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Queue Slurm Jobs
-pre_jid=$(sbatch --parsable --job-name=negcodeparrot_pre train.sbatch NegativeSOCodeParrot so pretrain_config lvwerra/codeparrot lm  "++training.max_steps=10000 ++training.log_steps=50 ++training.save_steps=250 ++training.eval_steps=250 ++training.num_train_epochs=1 ++task.dump_name='negative'")
+pre_jid=$(sbatch --parsable --job-name=negcodeparrot_pre train.sbatch NegativeSOCodeParrot so pretrain_config lvwerra/codeparrot lm  "++training.max_steps=10000 ++training.logging_steps=50 ++training.save_steps=250 ++training.eval_steps=250 ++training.num_train_epochs=1 ++task.dump_name='negative'")
 echo "Submitted PreTrain (id=$pretrain_jid)"
 train_jid=$(sbatch --parsable --dependency=afterok:$pre_jid --job-name=negcodeparrot1 \
   train.sbatch NegativeSOCodeParrot mbpp greene_config lvwerra/codeparrot lm \
