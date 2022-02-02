91a7b243ebef7fc7bdb0922b50e9f6f3b47e882b
Gabe <gabeorlanski@gmail.com>
2022-02-01 19:15:19 -0500

    Minor Changes

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -4,4 +4,4 @@
 parse_jid=$(sbatch parse_so.sbatch)
 sbatch --parsable --dependency=afterok:parse_jid --job-name=negcodeparrot \
   train.sbatch NegativeSOCodeParrot so lvwerra/codeparrot lm pretrain_config \
-  training.max_steps=11750 training.log_steps=50 training.save_steps=250 training.eval_steps=250
+  training.max_steps=10000 training.log_steps=50 training.save_steps=250 training.eval_steps=250
