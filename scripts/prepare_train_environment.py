7d19fe2e2a6e2e9241ce9bd4079afd1890c0ba98
Gabe <gabeorlanski@gmail.com>
2022-02-04 14:21:16 -0500

    New Conf

--- scripts/prepare_train_environment.py
+++ scripts/prepare_train_environment.py
@@ -7,17 +7,22 @@ import shutil
 import sys
 from pathlib import Path
 
+import yaml
+from omegaconf import OmegaConf
+
 if str(Path(__file__).parents[1]) not in sys.path:
     sys.path.insert(0, str(Path(__file__).parents[1]))
 from src.common import PROJECT_ROOT
 
 
-def prep_env(name, task, config, force_overwrite_dir, override_str, cfg_overrides):
-    group_name = task.upper()
-    for i in cfg_overrides + override_str.split(" "):
-        if 'group=' in i:
-            group_name = i.split('=')[-1]
-            break
+def prep_env(config,force_overwrite_dir):
+    cfg = OmegaConf.create(yaml.load(
+        PROJECT_ROOT.joinpath(config).open('r'),
+        yaml.Loader
+    ))
+    group_name = cfg.group
+    name = cfg.name
+    task = cfg.task.name
     print(f"Preparing train environment group={group_name}, "
           f"name={name}, and task={task}")
 
@@ -36,31 +41,14 @@ def prep_env(name, task, config, force_overwrite_dir, override_str, cfg_override
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("name", metavar="<Name of the Run>")
-    parser.add_argument("task", metavar="<Task to use>")
-    parser.add_argument("--config", help="Name of the base config file to use.",
-                        default='train_config')
+    parser.add_argument("config", help="Name of the base config file to use.")
     parser.add_argument('--force-overwrite-dir', '-force',
                         action="store_true",
                         default=False,
                         help="Force overwriting the directory if it exists.")
-    parser.add_argument('--override-str',
-                        help='Bash does not like lists of variable args. so '
-                             'pass as seperated list of overrides, seperated by ;.',
-                        default=''
-                        )
-    # This lets us have virtually the same exact setup as the hydra decorator
-    # without their annoying working directory and logging.
-    parser.add_argument('--hydra-overrides', '-hydra', nargs=argparse.REMAINDER,
-                        help='Everything after this argument is passed to the '
-                             'hydra config creator as an override command.')
 
     argv = parser.parse_args()
     prep_env(
-        argv.name,
-        argv.task,
         argv.config,
-        argv.force_overwrite_dir,
-        argv.override_str,
-        argv.hydra_overrides or []
+        argv.force_overwrite_dir
     )
