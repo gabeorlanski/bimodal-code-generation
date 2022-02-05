92c8b1094c38b7d72f576102bb88da5f8fa4cf6f
Gabe <gabeorlanski@gmail.com>
2022-02-05 11:17:19 -0500

    scripts

---
+++ scripts/gen_configs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Generate experiments
+singularity exec --overlay $SCRATCH/overlay-50G-10M.ext3:ro /scratch/work/public/singularity/cuda11.4.2-cudnn8.2.4-devel-ubuntu20.04.3.sif /bin/bash -c "
+source /ext3/env.sh
+conda activate adversarial-code
+python scripts/create_experiments.py experiment_cards.yaml conf -overwrite
+"
