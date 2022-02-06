aa4ee756f9bf0881e299346b27bb891f74621db2
Gabe <gabeorlanski@gmail.com>
2022-02-06 13:49:28 -0500

    d

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -2,5 +2,5 @@
 # Queue Slurm Jobs
 
 sbatch -J 'high_qual' make_splits.sbatch "python scripts/parse_so_data.py -out data/parsed_so parse data/stack_exchange/stackoverflow 32 high_qual -min '30' -val 0.01"
-sbatch -J 'general' make_splits.sbatch "python scripts/parse_so_data.py -out data/parsed_so parse data/stack_exchange/stackoverflow 32 general -val 0.01"
+sbatch -J 'general_parsing' make_splits.sbatch "python scripts/parse_so_data.py -out data/parsed_so parse data/stack_exchange/stackoverflow 32 general -val 0.01"
 sbatch -J 'exception' make_splits.sbatch "python scripts/parse_so_data.py -out data/parsed_so parse data/stack_exchange/stackoverflow 32 exceptions -contains exception,error,trace"
