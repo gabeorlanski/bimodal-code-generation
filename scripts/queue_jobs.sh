08eb4755a3872e2167754c62df8c60c57bf52f99
Gabe <gabeorlanski@gmail.com>
2022-02-07 07:03:16 -0500

    Dep

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # Queue Slurm Jobs
-sbatch --parsable --job-name=CodeParrotSmall_Exceptions_eval \
-	--dependency=afterok:$train_jid3 eval.sbatch \
+sbatch --parsable --job-name=CodeParrotSmall_Exceptions_eval eval.sbatch \
 	best_models/MBPP.CodeParrotSmall.Exceptions.FineTune validation,test 25
 
-sbatch --parsable --job-name=CodeParrotSmall_General_eval \
-	--dependency=afterok:$train_jid4 eval.sbatch \
+sbatch --parsable --job-name=CodeParrotSmall_General_eval  eval.sbatch \
 	best_models/MBPP.CodeParrotSmall.General.FineTune validation,test 25
-sbatch --parsable --job-name=CodeParrotSmall_HighQual_eval \
-	--dependency=afterok:$train_jid5 eval.sbatch \
+sbatch --parsable --job-name=CodeParrotSmall_HighQual_eval eval.sbatch \
 	best_models/MBPP.CodeParrotSmall.HighQual.FineTune validation,test 25
