cf6bae6ddfc1c3eb507b5d0fdaebdec4bde222c1
Gabe <gabeorlanski@gmail.com>
2022-02-08 16:38:36 -0500

    djdjdj

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -3,16 +3,16 @@
 rm -rf sbatch_logs
 mkdir "sbatch_logs"
 sbatch --job-name='codeparrot_exceptions_execute' \
-  eval_code.sbatch eval_outputs/MBPP MBPP.CodeParrotSmall.Exceptions.Finetune
+  eval_code.sbatch eval_results/MBPP MBPP.CodeParrotSmall.Exceptions.Finetune
 echo ""
 sbatch --job-name='codeparrot_general_execute' \
-  eval_code.sbatch eval_outputs/MBPP MBPP.CodeParrotSmall.General.FineTune
+  eval_code.sbatch eval_results/MBPP MBPP.CodeParrotSmall.General.FineTune
 echo ""
 
 sbatch --job-name='codeparrot_highqual_execute'  \
-  eval_code.sbatch eval_outputs/MBPP MBPP.CodeParrotSmall.HighQual.FineTune
+  eval_code.sbatch eval_results/MBPP MBPP.CodeParrotSmall.HighQual.FineTune
 echo ""
 
 sbatch --job-name='codeparrot_negative_execute' \
-  eval_code.sbatch eval_outputs/MBPP MBPP.CodeParrotSmall.Negative.FineTune
+  eval_code.sbatch eval_results/MBPP MBPP.CodeParrotSmall.Negative.FineTune
 echo ""
