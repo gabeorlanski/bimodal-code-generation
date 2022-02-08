2f8a9be824f35279b9ecc16df9e13dd80bc47924
Gabe <gabeorlanski@gmail.com>
2022-02-08 18:06:46 -0500

    Better get scores

--- scripts/get_scores.py
+++ scripts/get_scores.py
@@ -10,14 +10,13 @@ import sys
 # have import issues.
 if str(Path(__file__).parents[1]) not in sys.path:
     sys.path.insert(0, str(Path(__file__).parents[1]))
-from src.common import PROJECT_ROOT, setup_global_logging, flatten
-from src.config.experiments import load_composed_experiments_from_file
-from src.evaluation.code_eval import get_metrics_from_list
+from src.common import PROJECT_ROOT
 
 
 @click.command()
 @click.argument("dump_path")
-def main(dump_path):
+@click.argument("parsed_path")
+def main(dump_path, parsed_path):
     dump_path = PROJECT_ROOT.joinpath(dump_path)
     files = [
         'valid_answer_scores',
@@ -43,12 +42,39 @@ def main(dump_path):
         bins.append(stats['max'])
         score_bins = scores_series.groupby(pd.cut(scores_series, bins)).agg(['count'])
         for interval, value in score_bins['count'].iteritems():
-            stats[str(interval)] = int(value)
+            stats[f"Posts in interval {str(interval)}"] = int(value)
         print(f"Stats for {f}:")
         for k, v in stats.items():
             print(f"\t{k:>16} = {v:0.3f}")
         all_stats[f] = stats
 
+    parsed_path = PROJECT_ROOT.joinpath(parsed_path)
+    for file in parsed_path.glob('*.jsonl'):
+        print(f"Reading {file}")
+        line_counts = 0
+        question_scores = []
+        all_answer_scores = []
+        num_answers = []
+        for line in map(json.loads, file.open('r')):
+            question_scores.append(line['score'])
+            answer_scores = [a['score'] for a in line['answers'].values()]
+            num_answers.append(len(answer_scores))
+            all_answer_scores.extend(answer_scores)
+
+            line_counts += 1
+            if line_counts % 5000 == 0:
+                print(f"Finished line {line_counts}")
+
+        question_scores = pd.Series(question_scores)
+        all_answer_scores = pd.Series(all_answer_scores)
+        num_answers = pd.Series(num_answers)
+        all_stats[file.stem] = {
+            "question_scores": question_scores.describe().to_dict(),
+            "answer_scores"  : all_answer_scores.describe().to_dict(),
+            "num_answers"    : num_answers.describe().to_dict()
+        }
+        all_stats[file.stem]['num_samples'] = line_counts
+        all_stats[file.stem]['num_questions_with_answer'] = sum(num_answers>0)
     with dump_path.joinpath('stats.json').open('w') as f:
         json.dump(all_stats, f, indent=True)
 
