d506270c922c0956ff4863da164c13f1465fdbca
Gabe <gabeorlanski@gmail.com>
2022-02-09 11:44:47 -0500

    minor change

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -5,6 +5,5 @@ echo "Submitted Train (id=$train_jid0)"
 eval_jid0=$(sbatch --parsable --job-name=Uniform32Epoch.ParrotSmall_Negative_eval \
 	--dependency=afterok:$train_jid0 eval.sbatch \
 	best_models/MBPP.Uniform32Epoch.ParrotSmall.Negative.FineTune validation,test 100 "remove_input_ids=True")
-echo "Submitted Eval $eval_jid0 to run after $1(id=$train_jid0)"
-sbatch --job-name='Uniform32Epoch.ParrotSmall_Negative_execute' --dependency=afterok:$eval_jid0 eval_code.sbatch eval_results/MBPP MBPP.Uniform32Epoch.ParrotSmall.Negative.FineTune
+echo "Submitted Eval $eval_jid0 to run after (id=$train_jid0)"
 echo ""
