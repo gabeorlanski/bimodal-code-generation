d5e6cd90408aa0f30a965d52bd1ff287c5bd5a5c
Gabe <gabeorlanski@gmail.com>
2022-02-09 01:50:43 -0500

    minor change

--- scripts/batch_code_eval.sh
+++ scripts/batch_code_eval.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code MBPP.CodeParrotSmall.Negative.FineTune:v1 16
-bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code MBPP.CodeParrotSmall.HighQual.FineTune:v1 16
-bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code MBPP.CodeParrotSmall.General.FineTune:v1 16
-bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code MBPP.CodeParrotSmall.Exceptions.FineTune:v1 16
+bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code \
+  MBPP.Uniform.ParrotSmall.HighQual.FineTune-preds:v2 16
+bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code \
+  MBPP.Uniform.ParrotSmall.Exceptions.FineTune-preds:v0 16
+bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code \
+  MBPP.Uniform.ParrotSmall.General.FineTune-preds:v0 16
+bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code \
+  MBPP.Uniform.ParrotSmall.Negative.FineTune-preds:v0 16
