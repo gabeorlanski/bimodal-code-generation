5bf7f6c8f46e436ebc689e60ff36d01f63e7a9fe
Gabe <gabeorlanski@gmail.com>
2022-02-11 09:18:07 -0500

    minor change

--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 # Queue Slurm Jobs
 
-sbatch --job-name='FullData.ParrotSmall_Exceptions_execute' eval_code.sbatch eval_results/MBPP MBPP.FullData.ParrotSmall.Exceptions.FineTune
 
-sbatch --job-name='FullData.ParrotSmall_General_execute' eval_code.sbatch eval_results/MBPP MBPP.FullData.ParrotSmall.General.FineTune
+sbatch --job-name='Uniform.HypeParam.ParrotSmall_HighQual.LongSteps_execute' eval_code.sbatch eval_results/MBPP MBPP.Uniform.HypeParam.ParrotSmall.HighQual.LongSteps.FineTune
 
-sbatch --job-name='FullData.ParrotSmall_HighQual_execute' eval_code.sbatch eval_results/MBPP MBPP.FullData.ParrotSmall.HighQual.FineTune
 
-sbatch --job-name='FullData.ParrotSmall_Exceptions_execute' eval_code.sbatch eval_results/MBPP MBPP.FullData.ParrotSmall.Exceptions.FineTune
-
-sbatch --job-name='SODump10KSteps.32.ParrotSmall_General_execute'eval_code.sbatch eval_results/MBPP MBPP.SODump10KSteps.32.ParrotSmall.General.FineTune
-
-sbatch --job-name='SODump10KSteps.32.ParrotSmall_HighQual_execute' eval_code.sbatch eval_results/MBPP MBPP.SODump10KSteps.32.ParrotSmall.HighQual.FineTune
-sbatch --job-name='SODump10KSteps.32.ParrotSmall_Exceptions_execute' eval_code.sbatch eval_results/MBPP MBPP.SODump10KSteps.32.ParrotSmall.Exceptions.FineTune
-sbatch --job-name='SODump10KSteps.32.ParrotSmall_Negative_execute' eval_code.sbatch eval_results/MBPP MBPP.SODump10KSteps.32.ParrotSmall.Negative.FineTune
+sbatch --job-name='Uniform.HypeParam.ParrotSmall_HighQual.HighLR_execute' eval_code.sbatch eval_results/MBPP MBPP.Uniform.HypeParam.ParrotSmall.HighQual.HighLR.FineTune
