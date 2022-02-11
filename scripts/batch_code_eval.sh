801ebad740ac1e4b6659672c6eccef204a4a4a0f
Gabe <gabeorlanski@gmail.com>
2022-02-11 08:52:29 -0500

    minor change

--- scripts/batch_code_eval.sh
+++ scripts/batch_code_eval.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code \
-  MBPP.Uniform32Epoch.ParrotSmall.General.FineTune-preds:v0 16
-bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code \
-  MBPP.Uniform32Epoch.ParrotSmall.HighQual.FineTune-preds:v0 16
-bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code \
-  MBPP.Uniform32Epoch.ParrotSmall.Exceptions.FineTune-preds:v0 16
+bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code MBPP.SODump10KSteps.32.ParrotSmall.General.FineTune-preds:v0 16
+bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code MBPP.SODump10KSteps.32.ParrotSmall.Negative.FineTune-preds:v0 16
+bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code MBPP.SODump10KSteps.32.ParrotSmall.HighQual.FineTune-preds:v0 16
+bash scripts/docker_eval_code.sh nyu-code-research/adversarial-code MBPP.SODump10KSteps.32.ParrotSmall.Exceptions.FineTune-preds:v0 16
--- scripts/queue_jobs.sh
+++ scripts/queue_jobs.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 # Queue Slurm Jobs
-# Command for FullData.ParrotSmall_Negative
-train_jid0=$(sbatch --parsable --job-name=FullData.ParrotSmall_Negative_finetune train_single_gpu.sbatch generated_experiments/MBPP.FullData.ParrotSmall.Negative.FineTune.yaml)
-echo "Submitted Train (id=$train_jid0)"
-eval_jid0=$(sbatch --parsable --job-name=FullData.ParrotSmall_Negative_eval \
-	--dependency=afterok:$train_jid0 eval.sbatch \
-	best_models/MBPP.FullData.ParrotSmall.Negative.FineTune validation,test 100 "remove_input_ids=True")
-echo "Submitted Eval $eval_jid0 to run after $1(id=$train_jid0)"
-sbatch --job-name='FullData.ParrotSmall_Negative_execute' --dependency=afterok:$eval_jid0 eval_code.sbatch eval_results/MBPP MBPP.FullData.ParrotSmall.Negative.FineTune
-echo ""
 
-# Command for FullData.ParrotSmall_HighQual
-train_jid1=$(sbatch --parsable --job-name=FullData.ParrotSmall_HighQual_finetune train_single_gpu.sbatch generated_experiments/MBPP.FullData.ParrotSmall.HighQual.FineTune.yaml)
-echo "Submitted Train (id=$train_jid1)"
-eval_jid1=$(sbatch --parsable --job-name=FullData.ParrotSmall_HighQual_eval \
-	--dependency=afterok:$train_jid1 eval.sbatch \
-	best_models/MBPP.FullData.ParrotSmall.HighQual.FineTune validation,test 100 "remove_input_ids=True")
-echo "Submitted Eval $eval_jid1 to run after $1(id=$train_jid1)"
-sbatch --job-name='FullData.ParrotSmall_HighQual_execute' --dependency=afterok:$eval_jid1 eval_code.sbatch eval_results/MBPP MBPP.FullData.ParrotSmall.HighQual.FineTune
-echo ""
+sbatch --job-name='FullData.ParrotSmall_Exceptions_execute' eval_code.sbatch eval_results/MBPP MBPP.FullData.ParrotSmall.Exceptions.FineTune
+
+sbatch --job-name='FullData.ParrotSmall_General_execute' eval_code.sbatch eval_results/MBPP MBPP.FullData.ParrotSmall.General.FineTune
+
+sbatch --job-name='FullData.ParrotSmall_HighQual_execute' eval_code.sbatch eval_results/MBPP MBPP.FullData.ParrotSmall.HighQual.FineTune
+
+sbatch --job-name='FullData.ParrotSmall_Exceptions_execute' eval_code.sbatch eval_results/MBPP MBPP.FullData.ParrotSmall.Exceptions.FineTune
+
+sbatch --job-name='SODump10KSteps.32.ParrotSmall_General_execute'eval_code.sbatch eval_results/MBPP MBPP.SODump10KSteps.32.ParrotSmall.General.FineTune
+
+sbatch --job-name='SODump10KSteps.32.ParrotSmall_HighQual_execute' eval_code.sbatch eval_results/MBPP MBPP.SODump10KSteps.32.ParrotSmall.HighQual.FineTune
+sbatch --job-name='SODump10KSteps.32.ParrotSmall_Exceptions_execute' eval_code.sbatch eval_results/MBPP MBPP.SODump10KSteps.32.ParrotSmall.Exceptions.FineTune
+sbatch --job-name='SODump10KSteps.32.ParrotSmall_Negative_execute' eval_code.sbatch eval_results/MBPP MBPP.SODump10KSteps.32.ParrotSmall.Negative.FineTune
