eea8051606e25b8dadac3520cf6d64101d141602
Gabe <gabeorlanski@gmail.com>
2022-03-03 11:34:09 -0500

    minor change

--- src/data/parse_so/filtering.py
+++ src/data/parse_so/filtering.py
@@ -1,8 +1,10 @@
 import json
 import logging
+import os
 from collections import defaultdict, Counter
 
 import numpy as np
+import psutil
 from tqdm import tqdm
 import ujson
 from pathlib import Path
@@ -76,7 +78,10 @@ def create_filter_for_so_data(parsed_path, tag_filter_file, blacklist, debug, se
                 tag_files_to_get[question_dict['tag_to_use']].append(question_id)
                 questions_passing_filter += 1
         if finished % update_freq == 0:
-            logger.debug(f"{finished:>8}/{len(question_overview)} finished")
+            logger.info(f"{finished:>8}/{len(question_overview)} finished")
+            ram_pct = f"{psutil.virtual_memory()[2]:0.2f}%"
+            cpu_pct = f"{psutil.getloadavg()[-1] / os.cpu_count() * 100:0.2f}%"
+            logger.info(f"RAM Used={ram_pct:<6} | CPU Used={cpu_pct:<6}")
 
     logger.info(f"{len(tag_files_to_get)} tags to use.")
     logger.info(f"{questions_passing_filter} passed the filter")
