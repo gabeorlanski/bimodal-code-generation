565b527f64d160059140591017a2ff75f06a4290
Gabe <gabeorlanski@gmail.com>
2022-03-04 12:58:27 -0500

    shasd

--- make_config.sh
+++ make_config.sh
@@ -3,5 +3,5 @@
 singularity exec --overlay $SCRATCH/overlay-50G-10M.ext3:ro /scratch/work/public/singularity/cuda11.4.2-cudnn8.2.4-devel-ubuntu20.04.3.sif /bin/bash -c "
 source /ext3/env.sh
 conda activate adversarial-code
-python scripts/create_experiments.py ${1:-experiment_card} conf -overwrite
+python scripts/create_experiments.py ${1:-experiment_card} conf ${2:-''}
 "
