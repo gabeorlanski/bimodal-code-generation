f43a63dd19d7ad1e7c7a44cd40ea4db70819a617
Gabe <gabeorlanski@gmail.com>
2022-03-09 11:07:14 -0500

    test

--- make_config.sh
+++ make_config.sh
@@ -3,11 +3,11 @@
 singularity exec --overlay $SCRATCH/overlay-50G-10M.ext3:ro /scratch/work/public/singularity/cuda11.4.2-cudnn8.2.4-devel-ubuntu20.04.3.sif /bin/bash -c "
 source /ext3/env.sh
 conda activate adversarial-code
-if [ $2 -eq "0" ]
+if [ $# -gt 1 ];
   then
+    echo "Overwriting"
     python scripts/create_experiments.py ${1:-experiment_card} conf -overwrite
   else
     python scripts/create_experiments.py ${1:-experiment_card} conf
 fi
-
 "
