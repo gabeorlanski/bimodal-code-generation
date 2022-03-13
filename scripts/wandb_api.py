92d1369e7065b078fea3d63c33d24bdffef79ba8
Gabe <gabeorlanski@gmail.com>
2022-03-13 08:13:37 -0400

    added upload func to wandb api script

--- scripts/wandb_api.py
+++ scripts/wandb_api.py
@@ -15,19 +15,51 @@ if 'scripts' in str(PROJECT_ROOT):
         PROJECT_ROOT = PROJECT_ROOT.parent
 
 
-@click.command()
-def download_wandb_runs():
+@click.group()
+@click.pass_context
+def cli(ctx):
+    os.environ["WANDB_API_KEY"] = open('wandb_secret.txt').read().strip()
+    ctx.obj = {
+        'entity' : 'nyu-code-research',
+        'project': 'so-code-gen'
+    }
+
+
+@cli.command('upload_model')
+@click.argument('model_directory', metavar="<Model Directory or Checkpoint Dir>")
+@click.argument('run_id', metavar="<The Run ID to add the model too>")
+@click.pass_context
+def upload_model(ctx, model_directory, run_id):
+    model_directory = PROJECT_ROOT.joinpath(model_directory)
+    print(f"Uploading {model_directory} to {run_id}")
+
+    run = wandb.init(  # type: ignore
+        entity=ctx.obj['entity'],
+        project=ctx.obj['project'],
+        resume=True
+    )
+
+    print("Loaded run, now making artifact")
+    artifact = wandb.Artifact(name=f"model-{run.id}", type="model")  # type: ignore
+    artifact.add_dir(str(model_directory.resolve().absolute()))
+    run.log_artifact(artifact)
+    run.finish()
+
+
+@cli.command('download')
+@click.pass_context
+def download_wandb_runs(ctx):
     out_path = PROJECT_ROOT.joinpath('data/run_data')
     if not out_path.exists():
         out_path.mkdir(parents=True)
 
     print(f'Downloading to {out_path}')
-    api = wandb.Api()
+    api = wandb.Api()  # type: ignore
 
     print(f"Downloading runs")
 
     # Project is specified by <entity/project-name>
-    runs = api.runs("nyu-code-research/so-code-gen")
+    runs = api.runs(f"{ctx.obj['entity']}/{ctx.obj['project']}")
     print(f"{len(runs)} runs found")
     summary_keys = set()
     records = defaultdict(list)
@@ -55,7 +87,7 @@ def download_wandb_runs():
     for group_name, group_records in records.items():
         print(f"Saving {group_name}")
         runs_df = pd.DataFrame.from_records(group_records)
-        runs_df.to_json(out_path.joinpath(f"{group_name}.jsonl"),orient='records')
+        runs_df.to_json(out_path.joinpath(f"{group_name}.jsonl"), orient='records')
 
 
 if __name__ == '__main__':
