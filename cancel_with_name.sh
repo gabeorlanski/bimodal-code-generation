c8cac47946563d72e157c7327c5673e90e328562
Gabe <gabeorlanski@gmail.com>
2022-03-18 08:55:11 -0400

    asfd

--- cancel_with_name.sh
+++ cancel_with_name.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-files=(find  -maxdepth 1 -name "${1}*.sh")
 for f in generated_experiments/cancel_scripts/*.sh; do
-  if [[ "$f" == *"$1"* ]]; then
+  echo "$f ${1}"
+  if grep -q "$f" <<< "${1}";
     echo "$f"
   fi
 done
