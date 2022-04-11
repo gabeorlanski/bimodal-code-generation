921a831a2179de2ba5050dbbbf15c4d9c9dfb851
seon1890 <sy3420@nyu.edu>
2022-04-10 20:46:25 -0400

    pymc_parser.py adding this file

---
+++ src/tutorials/pymc_parser.py
@@ -0,0 +1,30 @@
+from typing import List, Tuple
+from bs4.element import Tag
+
+from .html_parsers import TutorialHTMLParser, TagType
+
+
+@TutorialHTMLParser.register('pymc')
+class LXMLParser(TutorialHTMLParser):
+    NAME = "PYMC"
+
+    def get_body(self, soup) -> Tag:
+        return soup.find(
+            'div',
+            {'class': 'ui container'}
+        )
+
+    def get_type_of_tag(self, tag: Tag) -> TagType:
+        tag_classes = tag.attrs.get('class', [])
+        if tag.name == 'div':
+            if 'section' in tag_classes:
+                return TagType.SECTION
+            elif 'pre' in tag_classes:
+                return TagType.CODE
+            elif 'p' in tag_classes:
+                return TagType.PARAGRAPH
+            return self.get_type_from_div_tag(tag)
+
+    def get_header_and_sections(self, tag) -> Tuple[List[Tag], List[Tag]]:
+
+        return [], tag.find_all('div', {'class': 'section'}, recursive=False)
