de5f5510bcb7c1fc1d6feba5d2cf477db57e30e4
Gabe <gabeorlanski@gmail.com>
2022-04-30 09:48:11 -0400

    NPV Debugging

--- src/training/training.py
+++ src/training/training.py
@@ -451,10 +451,10 @@ def train_model(cfg: DictConfig, train_args):
             for i, v in enumerate(validation_data):
                 if len(data_to_save) >= 25:
                     break
-
                 if v['input_ids'] is None or v['labels'] is None:
                     logger.warning(f"NONE FOUND at {i=}")
                     print(v)
+                    
                 data_to_save[i] = {
                     'input_ids': tokenizer.decode(v['input_ids']),
                     'labels'   : tokenizer.decode(v['labels'])
