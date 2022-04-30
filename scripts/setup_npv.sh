4e026813411e91952eebd4a55fb04944e785da22
Gabe <gabeorlanski@gmail.com>
2022-04-30 09:30:41 -0400

    Minor fixes to setup script and npv card

--- scripts/setup_npv.sh
+++ scripts/setup_npv.sh
@@ -2,14 +2,10 @@
 echo ""
 echo "===================================================="
 echo "Making Raw Data"
-python ./scripts/setup_datasets.py raw_npv test --negation -n ${2:-4} -gen data/generated_test.json
-python ./scripts/setup_datasets.py raw_npv train --negation -n ${2:-4}
-python ./scripts/setup_datasets.py raw_npv validation --negation -n ${2:-4}
+python ./scripts/setup_datasets.py $1 test --negation -n ${3:-4} -gen data/generated_test.json
 
 
 echo ""
 echo "===================================================="
 echo "Verifying Data"
-python ./scripts/setup_datasets.py verify_npv test --negation -n ${2:-4} -fratio ${1:-1}
-python ./scripts/setup_datasets.py verify_npv train --negation -n ${2:-4} -fratio ${1:-1}
-python ./scripts/setup_datasets.py verify_npv validation --negation -n ${2:-4} -fratio ${1:-1}
+python ./scripts/setup_datasets.py verify_npv test --negation -n ${3:-4} -fratio ${2:-1}
