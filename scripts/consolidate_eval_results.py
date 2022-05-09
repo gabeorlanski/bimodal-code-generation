5281979c0ded2e991ae7cef48fd7114cf4b44b8b
Gabe <gabeorlanski@gmail.com>
2022-05-09 09:56:26 -0400

    Analysis first go

---
+++ scripts/consolidate_eval_results.py
@@ -0,0 +1,96 @@
+import json
+import argparse
+import logging
+import random
+import shutil
+from collections import defaultdict, Counter
+from copy import deepcopy
+from pathlib import Path
+import sys
+from dataclasses import asdict
+from urllib.parse import urlparse, urljoin
+
+import psutil
+import ujson
+import yaml
+from lxml import etree
+import multiprocessing as mp
+
+from bs4 import BeautifulSoup
+import click
+import numpy as np
+from tqdm import tqdm
+import csv
+import tldextract
+from urllib.parse import urljoin
+import requests
+
+# If this file is called by itself (for creating the splits) then it will
+# have import issues.
+if str(Path(__file__).parents[1]) not in sys.path:
+    sys.path.insert(0, str(Path(__file__).parents[1]))
+from src.common import PROJECT_ROOT, setup_global_logging, flatten
+from src.evaluation.analysis import *
+
+
+@click.command()
+@click.argument('eval_dir')
+@click.option('--debug', is_flag=True, default=False, help='Enable Debug Mode')
+def consolidate_results(eval_dir, debug):
+    setup_global_logging(f"consolidate_eval", PROJECT_ROOT.joinpath('logs'),
+                         debug=debug)
+    logger = logging.getLogger("consolidate_eval")
+
+    eval_dir = PROJECT_ROOT.joinpath(eval_dir)
+    logger.info(f"Getting '{eval_dir}'")
+
+    mbpp_dir = eval_dir.joinpath('MBPP')
+    assert mbpp_dir.exists()
+    mbpp_eval_runs = {
+        p.name.split('_Eval')[0]: p
+        for p in mbpp_dir.glob('*')
+        if p.is_dir()
+    }
+    logger.info(f"Found {len(mbpp_eval_runs)} directories in  '{mbpp_dir}'")
+
+    human_eval_dir = eval_dir.joinpath('HUMAN_EVAL')
+    assert human_eval_dir.exists()
+    human_eval_runs = {
+        p.name.split('_HEEval')[0]: p
+        for p in human_eval_dir.glob('*')
+        if p.is_dir()
+    }
+    logger.info(f"Found {len(human_eval_runs)} directories in  '{human_eval_dir}'")
+
+    if set(human_eval_runs) != set(mbpp_eval_runs):
+        human_eval_missing = set(human_eval_runs).difference(set(mbpp_eval_runs))
+        mbpp_missing = set(mbpp_eval_runs).difference(set(human_eval_runs))
+        logger.warning(f'{len(human_eval_missing) + len(mbpp_missing)} Missing run(s):')
+        for r in mbpp_missing:
+            logger.warning(f"\tMBPP {r}")
+        for r in human_eval_missing:
+            logger.warning(f"\tHUMAN_EVAL {r}")
+
+    results = defaultdict(lambda: {'MBPP': {}, 'HUMAN_EVAL': {}})
+
+    for task_name, task_dict in {'MBPP': mbpp_eval_runs, 'HUMAN_EVAL': human_eval_runs}.items():
+        logger.info(f'Parsing {task_name} runs')
+        for run_name, path in tqdm(task_dict.items(), desc='Parsing'):
+            if not path.joinpath('test.jsonl'):
+                logger.error(f"{run_name} for task {task_name} is missing 'test.jsonl'")
+                continue
+            if not path.joinpath('execution_metrics.json'):
+                logger.error(f"{run_name} for task {task_name} "
+                             f"is missing 'execution_metrics.json'")
+                continue
+            run_results = parse_eval_results_dir(task_name, path)
+            results[run_name][task_name] = run_results
+
+    for task_name in ['MBPP', 'HUMAN_EVAL']:
+        with PROJECT_ROOT.joinpath('data', f'eval_{task_name}.jsonl').open('w') as f:
+            for run_name, run_results in results.items():
+                f.write(f"{json.dumps({'run_name': run_name, **run_results[task_name]})}\n")
+
+
+if __name__ == '__main__':
+    consolidate_results()
---
+++ src/evaluation/analysis.py
@@ -0,0 +1,82 @@
+import logging
+import json
+from collections import defaultdict, Counter
+from pathlib import Path
+
+import numpy as np
+
+logger = logging.getLogger(__name__)
+__all__ = [
+    "parse_eval_results_dir"
+]
+
+
+def parse_eval_results_dir(task, dir_path: Path):
+    logger.debug(f"Parsing {dir_path} from {task}")
+    logger.debug(f"Loading execution metrics from {dir_path}")
+
+    execution_metrics = json.loads(dir_path.joinpath('execution_metrics.json').read_text())
+
+    if 'test' not in execution_metrics:
+        raise KeyError(f"'test' not in {dir_path.joinpath('execution_metrics.json')}")
+
+    all_outcomes = set(execution_metrics['test']['outcome_pcts'])
+    results_by_task_id = execution_metrics['test']['results_by_task_id']
+    mean_tracker = defaultdict(list)
+
+    # Make sure that every one of the keys are present
+    task_result_counter = {k: 0 for k in [
+        'no_correct',
+        'all_correct',
+        'all_runtime_error',
+        'all_syntax_error',
+        'all_failed_tests',
+        'has_runtime_errors'
+    ]}
+    for tid, task_results in results_by_task_id.items():
+        total_preds = task_results['total']
+        if task_results['correct'] == 0:
+            task_result_counter['no_correct'] += 1
+
+        error_types = task_results['error_types']
+        syntax_errors = error_types.get('SyntaxError', 0)
+        failed_tests = error_types.get('Failed Tests', 0)
+        if syntax_errors == total_preds:
+            task_result_counter['all_syntax_error'] += 1
+        elif failed_tests == total_preds:
+            task_result_counter['all_failed_tests'] += 1
+        elif task_results['correct'] == total_preds:
+            task_result_counter['all_correct'] += 1
+
+        mean_tracker['correct'].append(task_results['correct'])
+        total_runtime_errors = 0
+        for outcome in all_outcomes:
+            if outcome == 'Correct':
+                continue
+
+            outcome_key = outcome.replace("_", '')
+            outcome_count = error_types.get(outcome.replace('_', ' '), 0)
+            if outcome_key not in ['TimedOut', 'SyntaxError', 'FailedTests']:
+                total_runtime_errors += outcome_count
+            mean_tracker[outcome_key].append(outcome_count)
+
+        if total_runtime_errors == total_preds:
+            task_result_counter['all_runtime_error'] += 1
+        mean_tracker['TotalRuntimeErrors'].append(total_runtime_errors)
+        if 'error_messages' in task_results and total_runtime_errors > 0:
+            task_result_counter['has_runtime_errors'] += 1
+            num_unique_errors = len(set(task_results['error_messages'].values()))
+            mean_tracker['UniqueErrors'].append(num_unique_errors)
+            mean_tracker['PCT_UniqueErrors'].append(
+                num_unique_errors / len(task_results['error_messages']) * 100
+            )
+    out = {}
+    for k, v in task_result_counter.items():
+        out[f"{k}_pct"] = v / len(results_by_task_id) * 100
+        out[f"{k}_total"] = v
+
+    for k, v in mean_tracker.items():
+        out[f"{k}_mean"] = np.mean(v)
+        out[f"{k}_std"] = np.std(v)
+
+    return out
